3c44abe38ef2b39a64310a559e2c883c87b5a0ee
Jan Feitsma <jan.feitsma@gmail.com>
2023-07-28 17:00:20 +0200

    implement new test function 'f' which does some simple actions (optional allocate, add arrays element-wise, calculate total sum, print timestamps), try to create good python bindings ...

--- my_package/pybind11/my_extension.cc
+++ my_package/pybind11/my_extension.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <pybind11/pybind11.h>
 
 #include "google/protobuf/message.h"
@@ -23,6 +24,54 @@ void MutateMessage(MyMessage* in_out) {
   in_out->set_my_string("I changed the value");
 }
 
+
+#include <google/protobuf/util/time_util.h>
+double GetCurrentTime() {
+    auto now = std::chrono::high_resolution_clock::now().time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::microseconds>(now).count() * 1e-6;
+}
+
+// Function that modifies the Protobuf object in-place
+void f(google::protobuf::Message* in_out, const google::protobuf::Message& in) {
+    std::cout << std::fixed << std::setw(17) << std::setprecision(6) << GetCurrentTime() << " c++ f start " << std::endl;
+    if (in_out) {
+
+        // some deprecated warnings ... meh.
+
+        const google::protobuf::Reflection* in_reflection = in.GetReflection();
+        const google::protobuf::FieldDescriptor* v_field = in.GetDescriptor()->FindFieldByName("v");
+        const google::protobuf::FieldDescriptor* k_field = in.GetDescriptor()->FindFieldByName("k");
+        const google::protobuf::FieldDescriptor* s_field = in.GetDescriptor()->FindFieldByName("s");
+
+        const auto& in_v = in_reflection->GetRepeatedField<int32_t>(in, v_field);
+        auto* in_out_v = in_out->GetReflection()->MutableRepeatedField<int32_t>(in_out, v_field);
+
+        while (in_out_v->size() < in_v.size()) { // TODO optimize
+            in_out_v->Add(0);
+        }
+        std::cout << std::fixed << std::setw(17) << std::setprecision(6) << GetCurrentTime() << " c++ f allocated " << std::endl;
+
+        for (int i = 0; i < in_v.size(); ++i) {
+            (*in_out_v)[i] += in_v.Get(i);
+        }
+
+        const int32_t in_k = in_reflection->GetInt32(in, k_field);
+        for (int k = 1; k < in_k; ++k) {
+            for (int i = 0; i < in_v.size(); ++i) {
+                (*in_out_v)[i] += in_v.Get(i);
+            }
+        }
+
+        int64_t total = 0;
+        for (int i = 0; i < in_out_v->size(); ++i) {
+            total += (*in_out_v)[i];
+        }
+
+        in_out->GetReflection()->SetInt64(in_out, s_field, total);
+    }
+    std::cout << std::fixed << std::setw(17) << std::setprecision(6) << GetCurrentTime() << " c++ f end " << std::endl;
+}
+
 PYBIND11_MODULE(my_extension, m) {
   pybind11_protobuf::ImportNativeProtoCasters();
   m.def("return_my_message", &ReturnMyMessage);
@@ -30,4 +79,17 @@ PYBIND11_MODULE(my_extension, m) {
   m.def("mutate_message", [](MyMessage in) {
     MutateMessage(&in);
     return in;
-  },  pybind11::arg("in")); }
+  },  pybind11::arg("in"));
+
+    // Define the C++ function 'f' using py::arg and py::call_guard
+    m.def("f",
+        [](MyMessage in_out, MyMessage in) {
+            f(&in_out, in);
+            return in_out;
+        },
+        pybind11::arg("in_out"),
+        pybind11::arg("in"),
+        pybind11::call_guard<py::gil_scoped_release>()
+    );
+}
+
