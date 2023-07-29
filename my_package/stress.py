036c01be9d3ca98fa284dbd004218f41de25f462
Jan Feitsma <jan.feitsma@gmail.com>
2023-07-29 12:28:37 +0200

    update stress.py to run both f and g

--- my_package/stress.py
+++ my_package/stress.py
@@ -7,31 +7,54 @@ import time
 
 
 
+
 def main():
-    N = 5000000
+    N = 10000000
     n = 60000
     k = 10
-    msg_add = MyMessage()
-    msg_add.n = n
-    msg_add.k = k
-    msg_add.s = 0
-    msg_total = MyMessage()
-    print('{:.6f} python init start'.format(time.time()))
-    msg_add.v.extend(range(1, 1+n))
-    msg_add.v.extend([0] * (N-n))
-    print('{:.6f} python init end'.format(time.time()))
+
+    msg = MyMessage()
+    msg.n = n
+    msg.k = k
+    msg.s = 0
+    msg.v.extend(range(1, 1+n))
+    msg.v.extend([0] * (N-n))
+
+    print('\ntesting function f (straightforward)\n')    
+    test_function(m.f, msg, False)
+
+    print('\ntesting function g (using memoryview)\n')    
+    test_function(m.g, msg, True)
+
+
+def test_function(func, msg, use_mview):
+    t0 = time.time()
+    print('{:.6f} python test start {:s}'.format(time.time(), func.__name__))
+
+    # the object to operate on is either msg or a memoryview
+    obj = msg
+    if use_mview:
+        message_bytes = msg.SerializeToString()
+        obj = memoryview(message_bytes)
+        print('{:.6f} python using memoryview of size {:d}'.format(time.time(), len(message_bytes)))
 
     # call the function, time it
     print('{:.6f} python call start'.format(time.time()))
-    t0 = time.time()
 
-    #m.f(msg_total, msg_add) # why does this not work
-    msg_total = m.f(msg_total, msg_add)
+    obj = func(obj) # each function uses inout ...
 
-    elapsed = time.time() - t0
     print('{:.6f} python call end'.format(time.time()))
 
-    print(f'N={N:<12d} n={n:<12d} k={k:<5d} s={msg_total.s:<20d}   elapsed={elapsed:7.3f}s')
+    if use_mview:
+        msg.ParseFromString(bytes(obj))
+    else:
+        msg = obj
+
+    print('{:.6f} python test end'.format(time.time()))
+
+    elapsed_total = time.time() - t0
+    N = len(msg.v)
+    print(f'N={N:<12d} n={msg.n:<12d} k={msg.k:<5d} s={msg.s:<20d}   elapsed={elapsed_total:7.3f}s')
 
 
 if __name__ == '__main__':
